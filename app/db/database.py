9ff93f64c7e84041d976316c7f42b3cdbee63246
andabele1 <112412492+andabele1@users.noreply.github.com>
2026-05-12 11:55:19 -0500

    fix database V2

--- app/db/database.py
+++ app/db/database.py
@@ -4,7 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = os.getenv(
-    "DATABASE_URL"
+    "DATABASE_URL",
+    "postgresql://yo:8jPBUgQ3sHo3tJSgXKLKEYtOkCuEJhCd@dpg-d81lkgho3t8c739edpbg-a/recetario_1ijf"
 )
 
 engine = create_engine(DATABASE_URL)
